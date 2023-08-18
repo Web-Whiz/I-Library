6fdee12e1b654f06bbc5e98b5231ef85b808a4a9
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-08-18 15:28:57 +0600

    added authproviders function

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,10 +1,59 @@
 import AuthContext from "@/Context/AuthContext";
+import auth from "@/firebase/firebase.auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import React, { useState } from "react";
 
 const AuthProviders = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const Login = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        setLoading(false);
+      } else {
+        setLoading(true);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
   const authInfo = {
     isMenuOpen,
     setIsMenuOpen,
@@ -12,6 +61,11 @@ const AuthProviders = ({ children }) => {
     setUser,
     loading,
     setLoading,
+    createUser,
+    Login,
+    googleSignIn,
+    logOut,
+    updateUserProfile,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
