414664ac1fc88076c387ac4514632e47b35f34a2
MuhammadFormanAli <muhammadformaanali@gmail.com>
2023-08-18 22:56:44 +0600

    created dashboard

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+
+import DashboardHome from '@/Page/Dashboard/DashboardHome/DashboardHome';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div> 
+            <DashboardHome />
+        </div>
+    );
+};
+
+export default page;
