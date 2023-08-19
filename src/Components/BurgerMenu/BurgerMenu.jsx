bfa977bdd4fdc7f5dbd4395efac70dfa75f7fedf
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-08-19 11:58:26 +0600

    update burger menu

--- src/Components/BurgerMenu/BurgerMenu.jsx
+++ src/Components/BurgerMenu/BurgerMenu.jsx
@@ -3,45 +3,89 @@ import React from "react";
 import logo from "@/assets/ILibrary.png";
 import { FaGoogle, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 import Link from "next/link";
+import {
+  BiHomeHeart,
+  BiBookAlt,
+  BiGridAlt,
+  BiUserCircle,
+  BiNews,
+  BiMessageRoundedDetail,
+  BiPhone,
+  BiInfoCircle,
+  BiSmile,
+} from "react-icons/bi";
 const BurgerMenu = () => {
   return (
-    <div className="w-full h-screen bg-white p-4 text-lg space-y-4 relative">
-      <div>
+    <div className="w-full h-screen bg-white text-lg space-y-4 relative">
+      <div className="p-3">
         <Image className="w-full h-fit" src={logo} alt="I Library" />
       </div>
-      <div className="py-2">
-        <form className="flex items-center text-sm">
-          <label>
-            <input
-              type="text"
-              className="outline-0 px-2 py-[6px] rounded-l-sm w-full bg-[#EFF3F8] border-b-2 border-indigo-500"
-              placeholder="write here to search"
-            />
-          </label>
-          <button className="bg-indigo-500 p-[6px] text-white border-b-2 border-indigo-500 rounded-r-sm">
+      <div className="py-2 px-2 w-full">
+        <form className="flex items-center text-sm w-full">
+          <input
+            type="text"
+            className="outline-0 px-2 py-[6px] rounded-l-sm w-full bg-[#EFF3F8] border-b-2 border-indigo-500"
+            placeholder="write here to search"
+          />
+          <button className="bg-indigo-500 p-[6px] text-white w-fit border-b-2 border-indigo-500 rounded-r-sm">
             search
           </button>
         </form>
       </div>
       <div>
-        <ul className="list-none py-1 text-center hover:text-indigo-600 font-medium text-[16px]">
-          <li>
-            <Link href="/">Home</Link>
+        <ul className="px-2 h-full bg-white text-[#3d3c3c]">
+          <li className="text-gray-900 font-medium cursor-pointer py-[6px] duration-200 text-lg hover:bg-violet-50 rounded-sm hover:text-violet-600  my-1 px-2">
+            <Link
+              href="/dashboard"
+              className="flex items-center gap-4 text-[15px]"
+            >
+              <BiHomeHeart /> Home
+            </Link>
           </li>
-          <li>
-            <Link href="/all-books">All Books</Link>
+          <li className="text-gray-900 font-medium cursor-pointer py-[6px] duration-200 text-lg hover:bg-violet-50 rounded-sm hover:text-violet-600  my-1 px-2">
+            <Link
+              href="dashboard/cart"
+              className="flex items-center gap-4 text-[15px]"
+            >
+              <BiBookAlt />
+              All Books
+            </Link>
           </li>
-          <li>
-            <Link href="/authors">Authors</Link>
+          <li className="text-gray-900 font-medium cursor-pointer py-[6px] duration-200 text-lg hover:bg-violet-50 rounded-sm hover:text-violet-600  my-1 px-2">
+            <a className="flex items-center gap-4 text-[15px]">
+              <BiGridAlt />
+              Category
+            </a>
           </li>
-          <li>
-            <Link href="/category">Categories</Link>
+          <li className="text-gray-900 font-medium cursor-pointer py-[6px] duration-200 text-lg hover:bg-violet-50 rounded-sm hover:text-violet-600  my-1 px-2">
+            <a className="flex items-center gap-4 text-[15px]">
+              <BiUserCircle /> Author
+            </a>
           </li>
-          <li>
-            <Link href="/blogs">Blogs</Link>
+          <li className="text-gray-900 font-medium cursor-pointer py-[6px] duration-200 text-lg hover:bg-violet-50 rounded-sm hover:text-violet-600  my-1 px-2">
+            <a className="flex items-center gap-4 text-[15px]">
+              <BiNews /> Publication
+            </a>
           </li>
-          <li>
-            <Link href="/contact">Contact</Link>
+          <li className="text-gray-900 font-medium cursor-pointer py-[6px] duration-200 text-lg hover:bg-violet-50 rounded-sm hover:text-violet-600 my-1  px-2">
+            <a className="flex items-center gap-4 text-[15px]">
+              <BiMessageRoundedDetail /> Blogs
+            </a>
+          </li>
+          <li className="text-gray-900 font-medium cursor-pointer py-[6px] duration-200 text-lg hover:bg-violet-50 rounded-sm hover:text-violet-600  my-1 px-2 ">
+            <a className="flex items-center gap-4 text-[15px]">
+              <BiPhone /> Contact
+            </a>
+          </li>
+          <li className="text-gray-900 font-medium cursor-pointer py-[6px] duration-200 text-lg hover:bg-violet-50 rounded-sm hover:text-violet-600 my-1  px-2">
+            <a className="flex items-center gap-4 text-[15px]">
+              <BiInfoCircle /> About us
+            </a>
+          </li>
+          <li className="text-gray-900 font-medium cursor-pointer py-[6px] duration-200 text-lg hover:bg-violet-50 rounded-sm hover:text-violet-600 my-1  px-2">
+            <a className="flex items-center gap-4 text-[15px]">
+              <BiSmile /> Kids zone
+            </a>
           </li>
         </ul>
       </div>
