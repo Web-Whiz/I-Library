3fd1f49cd7b410c4a215f0e8d1b4bb550e0fe7da
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-08-19 20:15:46 +0600

    add google login and email password login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,16 +1,34 @@
+"use client";
 import Link from "next/link";
-import { BsGoogle } from "react-icons/bs";
+import { FcGoogle } from "react-icons/fc";
 import { FaFacebookF } from "react-icons/fa";
 import login from "@/assets/login.png";
-import {
-  MdLock,
-  MdEmail,
-  MdOutlineMailOutline,
-  MdOutlineLockOpen,
-} from "react-icons/md";
+import { MdOutlineMailOutline, MdOutlineLockOpen } from "react-icons/md";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
+import useAuth from "@/Utils/useAuth";
 
 const LoginPage = () => {
+  const { Login, googleSignIn, user } = useAuth();
+  const { register, handleSubmit } = useForm();
+  const handleLogin = (data) => {
+    console.log(data);
+    Login(data.email, data.password)
+      .then((result) => {
+        console.log(result.user);
+        console.log(user);
+      })
+      .catch((error) => console.log(error));
+  };
+  const handleGoogleLogin = () => {
+    googleSignIn()
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="bg-[#f3f4f6]  max-w-[700px] mx-auto border md:shadow-md">
       {/* Login Banner */}
@@ -30,13 +48,14 @@ const LoginPage = () => {
 
       <div className="p-6 md:p-12">
         <h1 className="text-center my-8 text-3xl font-bold">Login Here </h1>
-        <form>
+        <form onSubmit={handleSubmit(handleLogin)}>
           <div className="flex items-center my-10 ">
             <span className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
               <MdOutlineMailOutline className="text-gray-400 text-lg"></MdOutlineMailOutline>
             </span>
             <input
               className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+              {...register("email", { required: true })}
               placeholder="johnsmith@example.com"
               type="email"
               required
@@ -50,6 +69,7 @@ const LoginPage = () => {
             <input
               className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
               placeholder="************"
+              {...register("password", { required: true })}
               type="password"
               required
             />
@@ -67,16 +87,20 @@ const LoginPage = () => {
               </Link>
             </p>
           </div>
-          <button className="text-xl text-white p-2 bg-indigo-700 font-semibold w-full">
+          <button
+            type="submit"
+            className="text-xl text-white p-2 bg-indigo-700 font-semibold w-full"
+          >
             Sign in
           </button>
         </form>
         <div className="flex flex-col sm:flex-row justify-between gap-8 my-8 text-center">
-          <button className="flex items-center justify-center text-lg p-2 w-full font-bold bg-blue-950 text-center ">
-            <BsGoogle className="text-white text-3xl " />
+          <button onClick={handleGoogleLogin} className="flex items-center gap-2 hover:bg-[#f3f4f6] hover:-translate-y-0.5 justify-center text-lg p-2 w-full font-bold bg-white shadow-md text-center ">
+            <FcGoogle className="text-2xl" /> <span>Google</span>
           </button>
-          <button className="flex items-center justify-center text-lg p-2 w-full font-bold bg-blue-950 text-center ">
-            <FaFacebookF className="text-white text-3xl " />
+          <button className="flex items-center gap-2 hover:bg-[#f3f4f6] hover:-translate-y-0.5 justify-center text-lg p-2 w-full font-bold bg-white shadow-md text-center ">
+            <FaFacebookF className="text-[#1877F2] text-2xl" />
+            <span>Facebook</span>
           </button>
         </div>
       </div>
