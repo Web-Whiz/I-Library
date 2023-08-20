8d349851c4ca4a0529b6609f87348203fac3d6f0
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-08-20 11:26:29 +0600

    update dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,130 +1,162 @@
+import Image from "next/image";
 import React from "react";
-
+import profilePhoto from "@/assets/Author01.jpg";
 const page = () => {
   return (
-    <div className="w-full bg-white">
-      <div className="bg-indigo-700 p-3 rounded-md flex flex-col md:flex-row gap-4 items-center">
-        <h1 className="font-bold text-2xl text-white">Personal Information</h1>
-        <p className="text-black cursor-pointer font-semibold hover:underline">
-          Change Information
-        </p>
+    <div className="w-full bg-white shadow-lg p-5 md:p-8">
+      {/* personal information */}
+      <div className="grid md:grid-cols-5 gap-4 items-start border-b-[1px] border-gray-200 pb-8">
+        <div className="md:col-span-2">
+          <h2 className="text-xl sm:text-2xl font-serif font-semibold">
+            Personal Information
+          </h2>
+          <p className="text-[16px]">
+            Use a permanent address where you can receive mail.
+          </p>
+        </div>
+        <div className="md:col-span-3">
+          {/* Profile photo */}
+          <div className="flex items-center gap-6">
+            <Image
+              width={100}
+              className="rounded-md"
+              src={profilePhoto}
+              alt="profile photo"
+            />
+            <div>
+              <label
+                htmlFor="uploadPhoto"
+                className="text-[15px] cursor-pointer font-medium px-3 py-[6px] rounded-md bg-violet-100 text-violet-600"
+              >
+                Change avatar
+              </label>
+              <input className="hidden" type="file" id="uploadPhoto" />
+              <p className="text-[12px] mt-2">JPG, GIF, or PNG. 1MB Max.</p>
+            </div>
+          </div>
+          {/* profile photo */}
+          <form className="mt-4">
+            <div>
+              <label htmlFor="name" className="text-[16px] font-medium">
+                Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-full px-3 py-[6px] text-[16px] mt-[6px] rounded-md border-2 border-gray-200 outline-none focus:border-indigo-500"
+                placeholder="Type your name"
+              />
+            </div>
+            <div className="mt-3">
+              <label htmlFor="Email" className="text-[16px] font-medium">
+                Email:
+              </label>
+              <input
+                type="text"
+                id="Email"
+                className="w-full px-3 py-[6px] text-[16px] mt-[6px] rounded-md border-2 border-gray-200 outline-none focus:border-indigo-500"
+                placeholder="Type your email"
+              />
+            </div>
+            <div className="mt-3">
+              <label htmlFor="phone" className="text-[16px] font-medium">
+                Phone:
+              </label>
+              <input
+                type="number"
+                id="phone"
+                className="w-full px-3 py-[6px] text-[16px] mt-[6px] rounded-md border-2 border-gray-200 outline-none focus:border-indigo-500"
+                placeholder="Type your phone number"
+              />
+            </div>
+            <div className="flex justify-end mt-5">
+              <button className="text-[16px] rounded-md font-semibold bg-violet-100 text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase">
+                save
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-
-      <div>
-        <h4 className="mt-10">Profile Picture</h4>
-        <img
-          className="avatar w-24 rounded-full"
-          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=200&q=80"
-        />
+      {/* personal information */}
+      {/* change password */}
+      <div className="grid md:grid-cols-5 gap-4 items-start border-b-[1px] border-gray-200 py-8">
+        <div className="md:col-span-2">
+          <h2 className="text-xl sm:text-2xl font-serif font-semibold">
+            Change password
+          </h2>
+          <p className="text-[16px]">
+            Update your password associated with your account.
+          </p>
+        </div>
+        <div className="md:col-span-3">
+          <form className="mt-4">
+            <div>
+              <label
+                htmlFor="currentPassword"
+                className="text-[16px] font-medium"
+              >
+                Current password:
+              </label>
+              <input
+                type="text"
+                id="currentPassword"
+                className="w-full px-3 py-[6px] text-[16px] mt-[6px] rounded-md border-2 border-gray-200 outline-none focus:border-indigo-500"
+                placeholder="Enter current password"
+              />
+            </div>
+            <div className="mt-3">
+              <label htmlFor="newPassword" className="text-[16px] font-medium">
+                New password:
+              </label>
+              <input
+                type="text"
+                id="newPassword"
+                className="w-full px-3 py-[6px] text-[16px] mt-[6px] rounded-md border-2 border-gray-200 outline-none focus:border-indigo-500"
+                placeholder="Type new password"
+              />
+            </div>
+            <div className="mt-3">
+              <label
+                htmlFor="confirmPassword"
+                className="text-[16px] font-medium"
+              >
+                Confirm password:
+              </label>
+              <input
+                type="text"
+                id="confirmPassword"
+                className="w-full px-3 py-[6px] text-[16px] mt-[6px] rounded-md border-2 border-gray-200 outline-none focus:border-indigo-500"
+                placeholder="Retype confirm password"
+              />
+            </div>
+            <div className="flex justify-end mt-5">
+              <button className="text-[16px] rounded-md font-semibold bg-violet-100 text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase">
+                change password
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-
-      <form className="text-black flex flex-col gap-4">
-        <label htmlFor="name">Name</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="text"
-          name="name"
-          id="name"
-          defaultValue="John Doe"
-        />
-
-        <label htmlFor="email">Email</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="email"
-          name="email"
-          id="email"
-          defaultValue="john@doe.com"
-        />
-
-        <label htmlFor="phone">Phone</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="number"
-          name="phone"
-          id="phone"
-          defaultValue="01003082245"
-        />
-
-        <label htmlFor="dateOfBirth">Date Of Birth</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="text"
-          name="dateOfBirth"
-          id="dateOtBirth"
-          defaultValue="01.01.01"
-        />
-        <label htmlFor="name">Name</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="text"
-          name="name"
-          id="name"
-          defaultValue="John Doe"
-        />
-
-        <label htmlFor="email">Email</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="email"
-          name="email"
-          id="email"
-          defaultValue="john@doe.com"
-        />
-
-        <label htmlFor="phone">Phone</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="number"
-          name="phone"
-          id="phone"
-          defaultValue="01003082245"
-        />
-
-        <label htmlFor="dateOfBirth">Date Of Birth</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="text"
-          name="dateOfBirth"
-          id="dateOtBirth"
-          defaultValue="01.01.01"
-        />
-        <label htmlFor="name">Name</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="text"
-          name="name"
-          id="name"
-          defaultValue="John Doe"
-        />
-
-        <label htmlFor="email">Email</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="email"
-          name="email"
-          id="email"
-          defaultValue="john@doe.com"
-        />
-
-        <label htmlFor="phone">Phone</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="number"
-          name="phone"
-          id="phone"
-          defaultValue="01003082245"
-        />
-
-        <label htmlFor="dateOfBirth">Date Of Birth</label>
-        <input
-          className="w-full p-2 font-bold outline-none bg-white rounded-md"
-          type="text"
-          name="dateOfBirth"
-          id="dateOtBirth"
-          defaultValue="01.01.01"
-        />
-      </form>
+      {/* change password */}
+      {/* delete my account */}
+      <div className="grid md:grid-cols-5 gap-4 items-center py-8">
+        <div className="md:col-span-2">
+          <h2 className="text-xl sm:text-2xl font-serif font-semibold">
+            Delete account
+          </h2>
+          <p className="text-[16px]">
+            No longer want to use our service? You can delete your account here.
+            This action is not reversible. All information related to this
+            account will be deleted permanently.
+          </p>
+        </div>
+        <div className="md:col-span-3">
+          <button className="text-[16px] rounded-md font-semibold bg-red-100 text-red-600 hover:bg-red-600 duration-200 hover:text-red-100 px-4 py-[6px] uppercase">
+            Yes, delete my account
+          </button>
+        </div>
+      </div>
+      {/* delete my account */}
     </div>
   );
 };
