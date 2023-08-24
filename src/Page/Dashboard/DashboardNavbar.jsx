9053a6b795749aa8cd737123ca569a959524203f
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-08-24 12:35:28 +0600

    resolve all books data get issue

--- src/Page/Dashboard/DashboardNavbar.jsx
+++ src/Page/Dashboard/DashboardNavbar.jsx
@@ -16,6 +16,7 @@ import {
   BiBookAlt,
   BiGridAlt,
   BiSolidDashboard,
+  BiSolidBookAlt,
 } from "react-icons/bi";
 import useAuth from "@/Utils/useAuth";
 const DashboardNavbar = () => {
@@ -69,9 +70,9 @@ const DashboardNavbar = () => {
           </a>
         </li>
         <li className="text-gray-900 font-medium cursor-pointer py-[6px] duration-200 text-lg hover:bg-violet-50 rounded-sm hover:text-violet-600  my-1 px-2">
-          <a className="flex items-center gap-4 text-[15px]">
-            <BiListCheck /> My LIst{" "}
-          </a>
+          <Link href="/dashboard/requested-books" className="flex items-center gap-4 text-[15px]">
+            <BiSolidBookAlt /> Requested Books
+          </Link>
         </li>
         <li className="text-gray-900 font-medium cursor-pointer py-[6px] duration-200 text-lg hover:bg-violet-50 rounded-sm hover:text-violet-600 my-1  px-2">
           <a className="flex items-center gap-4 text-[15px]">
--- src/Utils/useBooks.jsx
+++ src/Utils/useBooks.jsx
@@ -1,5 +1,15 @@
-export const getBooks = async () => {
-  const res = await fetch("http://localhost:5000/books");
-  const data = res.json();
-  return data;
+"use client";
+import { useQuery } from "@tanstack/react-query";
+
+export const getBooks = () => {
+  const { data: allBooks = [], refetch } = useQuery({
+    queryKey: ["all books"],
+    // enabled: !loading,
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/books`);
+      return res.json();
+    },
+  });
+
+  return [allBooks, refetch];
 };
