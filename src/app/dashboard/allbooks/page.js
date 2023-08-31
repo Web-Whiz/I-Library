24129665fbfbcbef814d1d93dc2a17845599d081
sibom007 <vaispark400@gmail.com>
2023-08-31 23:55:51 +0600

    new add responsive

--- src/app/dashboard/allbooks/page.js
+++ src/app/dashboard/allbooks/page.js
@@ -52,161 +52,59 @@ const page = () => {
   };
 
   return (
-    <div>
-      <div>
-        <div className="form-control">
-          <div className="flex lg:ml-48 md:ml-0">
-            <input
-              type="text"
-              onChange={(e) => setsesrch(e.target.value)}
-              placeholder="Enter title of books Search… "
-              className="w-full md:w-[530px] h-12 rounded border-2 border-b-blue-600 p-4 mb-5 md:mb-2 mt-5"
+    <div className="bg-white w-full shadow-lg p-5">
+      <div className="flex items-center my-5">
+        <input
+          type="text"
+          onChange={(e) => setsesrch(e.target.value)}
+          className="outline-0 px-3 py-2 rounded-l-sm w-[230px] sm:w-[350px] md:w-[350px] lg:w-[350px] xl:w-[350px] bg-[#EFF3F8] border-b-2 border-indigo-500"
+          placeholder="write here to search"
+        />
+        <button
+          className="bg-indigo-700 p-2 text-white border-b-2 border-indigo-500 rounded-r-sm"
+          onClick={Handlersearch}
+        >
+          Search...
+        </button>
+      </div>
+
+      {Booksdatas.map((booksdata) => (
+        <div
+          key={booksdata._id}
+          className="lg:flex md:flex sm:flex items-center justify-between rounded mt-7 bg-blue-300 shadow-md p-4"
+        >
+          <div className="lg:flex block items-center gap-5">
+            <img
+              className="md:w-24 md:h-24 lg:w-24 lg:h-24 sm:w-24 sm:h-24 w-full h-[400px] rounded-md"
+              src={booksdata.image_url}
+              alt=""
             />
+            <h1 className="sm:mt-2 sm:break-words sm:w-40 mt-2">
+              <span className="font-semibold ">Name: </span>
+              {booksdata.title}
+            </h1>
+          </div>
+
+          <div className="flex py-2 items-center gap-5 ">
+            <p>{booksdata.added_date}</p>
+            <p>{booksdata.published_in}</p>
+          </div>
+          <div className="md:flex lg:block lg:space-x-2 sm:block flex items-center gap-5 sm:space-y-3 md:space-y-0 lg:space-y-0">
+            <button className="md:inline-flex block text-[13px] rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 p-2 uppercase">
+              <Link href={`/all-books/${booksdata._id}`}>details</Link>
+            </button>
+            <button className="md:inline-flex block text-[13px] rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase">
+              <Link href={`/dashboard/allbooks/${booksdata._id}`}>update</Link>
+            </button>
             <button
-              className="bg-indigo-700 text-white outline-none px-2 mb-5 md:mb-2 mt-5"
-              onClick={Handlersearch}
+              onClick={() => handlerdelete(booksdata._id)}
+              className="md:inline-flex block text-[13px] rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase"
             >
-              Search...
+              Delete
             </button>
           </div>
         </div>
-      </div>
-      <div>
-        <div className="md:hidden lg:block hidden">
-          <div>
-            <div className="flex w-[1240px] ml-[190px] bg-white mt-5 border-2 p-2">
-              <p className="ml-6">Photo</p>
-              <p className="ml-[110px]">Title</p>
-              <p className="ml-[250px]">Date</p>
-              <p className="ml-[100px]">Publish</p>
-              <p className="ml-[180px]">Detail</p>
-              <p className="ml-[80px]">Update</p>
-              <p className="ml-[70px]">Delete</p>
-            </div>
-            {Booksdatas.map((booksdata) => (
-              <div
-                key={booksdata._id}
-                className="flex rounded mt-7 bg-blue-300 lg:w-[1240px] ml-48 shadow-md"
-              >
-                <div className="flex items-center p-5 space-x-3">
-                  <img
-                    className="w-24 h-24 rounded-md"
-                    src={booksdata.image_url}
-                    alt=""
-                  />
-                  <h1 className="w-56">{booksdata.title}</h1>
-                  <div className="flex space-x-20">
-                    <p className="ml-[80px]">{booksdata.added_date}</p>
-                    <p>{booksdata.published_in}</p>
-                  </div>
-                  <div className="flex space-x-10">
-                    <button className="ml-[170px] text-[13px]  rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 p-2 uppercase">
-                      <Link href={`/all-books/${booksdata._id}`}>details</Link>
-                    </button>
-                    <button className="text-[13px] ml-2 rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase">
-                      <Link href={`/dashboard/allbooks/${booksdata._id}`}>
-                        update
-                      </Link>
-                    </button>
-                    <button
-                      onClick={() => handlerdelete(booksdata._id)}
-                      className="text-[13px] ml-2 rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase"
-                    >
-                      Delete
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        {/* i pad */}
-
-        <div className="md:block hidden lg:hidden">
-          {Booksdatas.map((booksdata) => (
-            <div
-              key={booksdata._id}
-              className="bg-base-100 shadow-xl rounded mb-5 flex p-5"
-            >
-              <figure>
-                <img
-                  className="w-[43px] rounded"
-                  src={booksdata.image_url}
-                  alt="Movie"
-                />
-              </figure>
-              <div className="p-3 flex">
-                <h2 className="card-title w-36">{booksdata.title}</h2>
-                <p className="ml-20">{booksdata.added_date}</p>
-                <div className="flex ml-10">
-                  <button className="text-[13px]  rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase">
-                    <Link href={`/all-books/${booksdata._id}`}>
-                      view details
-                    </Link>
-                  </button>
-
-                  <button className="text-[13px] ml-2 rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase">
-                    <Link href={`/dashboard/allbooks/${booksdata._id}`}>
-                      update
-                    </Link>
-                  </button>
-
-                  <button
-                    onClick={() => handlerdelete(booksdata._id)}
-                    className="text-[13px] ml-2 rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        {/* Phone responsive */}
-
-        <div className="md:hidden lg:hidden block sm:grid sm:grid-cols-2 sm:ml-5  ml-4 w-[328px] sm:w-full">
-          {Booksdatas.map((booksdata) => (
-            <div
-              key={booksdata._id}
-              className="shadow-xl rounded mb-5 "
-            >
-              <figure>
-                <img
-                  className="w-[343px] h-[300px]"
-                  src={booksdata.image_url}
-                  alt="Movie"
-                />
-              </figure>
-              <div className="p-3">
-                <h2 className="card-title">{booksdata.title}</h2>
-                <p>{booksdata.added_date}</p>
-                <div className="flex">
-                  <button className="text-[13px]  rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase">
-                    <Link href={`/all-books/${booksdata._id}`}>
-                      view details
-                    </Link>
-                  </button>
-
-                  <button className="text-[13px] ml-2 rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase">
-                    <Link href={`/dashboard/allbooks/${booksdata._id}`}>
-                      update
-                    </Link>
-                  </button>
-
-                  <button
-                    onClick={() => handlerdelete(booksdata._id)}
-                    className="text-[13px] ml-2 rounded-md font-semibold bg-white text-violet-600 hover:bg-violet-600 duration-200 hover:text-violet-100 px-4 py-[6px] uppercase"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
